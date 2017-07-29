8acd36804dc3b4d32353db38717d92a2a1eaae50
ZephyrRaine <zephyr.raine@gmail.com>
2017-07-29 18:12:16 +0200

    lots of stuff + proper hierarchy

---
+++ Assets/Scripts/MultiDebug.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum DEBUG_TABS
+{
+    PERFORMANCES,
+    STORY,
+    SCENE
+}
+
+public class MultiDebug : MonoBehaviour
+{
+    [SerializeField]
+    private bool displayDebugInfo = true;
+    bool uiActive = true;
+    
+    
+    static void EasyCreate()
+    {
+        if(Debug.isDebugBuild)
+        {
+            MultiDebug md = FindObjectOfType<MultiDebug>();
+            if(md == null)
+            {
+                GameObject go = new GameObject("[MultiDebug]", typeof(MultiDebug));
+                DontDestroyOnLoad(go);
+            }
+        }
+    }
+    
+    void OnGUI()
+    {
+        if (displayDebugInfo = GUI.Toggle(new Rect(10, 10, 200, 30), displayDebugInfo, "Display debug info"))
+        {
+			
+        }
+    }
+    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D)) displayDebugInfo = !displayDebugInfo;
+    }
+
+    void LateUpdate()
+    {
+       
+    }
+}
---
+++ Assets/Scripts/Wiggle.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wiggle : MonoBehaviour {
+
+    float _speed;
+
+    Vector3 _randomDir;
+    float _elapsed;
+
+    float _ratioAngle;
+    float _ratioScale;
+    // Use this for initialization
+    void Start () 
+	{
+        _elapsed = Random.Range(-Mathf.PI * 2, Mathf.PI * 2);
+        _randomDir = new Vector3(Mathf.Cos(Random.value * Mathf.PI * 2), Mathf.Sin(Random.value * Mathf.PI * 2)).normalized;
+        _speed = Random.Range(-2f, 2f);
+        _ratioAngle = Random.Range(.2f, .8f);
+        _ratioScale = Random.Range(.2f, .8f);
+    }
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        _elapsed += Time.deltaTime * 0.5f;	
+        transform.position += new Vector3(Mathf.Cos(_elapsed), Mathf.Sin(_elapsed))*2f+_randomDir*_speed*Mathf.Cos(_elapsed);
+        transform.eulerAngles += new Vector3(0f, 0f, 1f) * Mathf.Sin(_elapsed)*_ratioAngle;
+        Vector2 ok = Vector2.one * (0.25f + (Mathf.Cos(_elapsed)*0.5f+0.5f) * _ratioScale);
+        transform.localScale = new Vector3(ok.x, ok.y, 1f);
+    }
+}
