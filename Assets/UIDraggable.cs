7bfa163228677266e8ad8015611391dd41c36157
ZephyrRaine <zephyr.raine@gmail.com>
2017-07-29 13:32:41 +0200

    here we go yeah

--- Assets/UIDraggable.cs
+++ Assets/UIDraggable.cs
@@ -5,12 +5,17 @@ using UnityEngine.EventSystems;
 public class UIDraggable : UIComponent, IBeginDragHandler, IEndDragHandler, IDragHandler, IDropHandler {
 
     Vector2 _startPosition;
+    Transform _parent;
+    int _siblingOrder;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
         _startedDragging = true;
         _startPosition = transform.position;
         _image.raycastTarget = false;
+        _parent = transform.parent;
+        _siblingOrder = transform.GetSiblingIndex();
+        transform.SetParent(_parent.parent);
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -18,6 +23,8 @@ public class UIDraggable : UIComponent, IBeginDragHandler, IEndDragHandler, IDra
         _startedDragging = false;
 		transform.position = _startPosition;
         _image.raycastTarget = true;
+        transform.SetParent(_parent);
+        transform.SetSiblingIndex(_siblingOrder);
     }
 
     public void OnDrag(PointerEventData eventData)
