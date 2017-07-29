6fe69e6836d95b2a2c6f164df4490960dea5e931
ZephyrRaine <zephyr.raine@gmail.com>
2017-07-29 14:30:04 +0200

    click toolbar instead of yknow

--- Assets/UIDraggable.cs
+++ Assets/UIDraggable.cs
@@ -8,6 +8,8 @@ public class UIDraggable : UIComponent, IBeginDragHandler, IEndDragHandler, IDra
     Transform _parent;
     int _siblingOrder;
 
+    GameObject _placeHolder;
+
     public void OnBeginDrag(PointerEventData eventData)
     {
         _startedDragging = true;
@@ -15,8 +17,13 @@ public class UIDraggable : UIComponent, IBeginDragHandler, IEndDragHandler, IDra
         _image.raycastTarget = false;
         _parent = transform.parent;
         _siblingOrder = transform.GetSiblingIndex();
+
         transform.SetParent(_parent.parent);
         eventData.selectedObject = gameObject;
+
+        _placeHolder = new GameObject();
+        _placeHolder.transform.SetParent(_parent);
+        _placeHolder.transform.SetSiblingIndex(_siblingOrder);
     }
 
     public void OnEndDrag(PointerEventData eventData)
@@ -26,6 +33,7 @@ public class UIDraggable : UIComponent, IBeginDragHandler, IEndDragHandler, IDra
         _image.raycastTarget = true;
         transform.SetParent(_parent);
         transform.SetSiblingIndex(_siblingOrder);
+        Destroy(_placeHolder);
     }
 
     public void OnDrag(PointerEventData eventData)
