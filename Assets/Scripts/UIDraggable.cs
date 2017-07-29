d5aaa8df8112bcd8259c354c184d826f3f8debcd
ZephyrRaine <zephyr.raine@gmail.com>
2017-07-29 21:37:56 +0200

    I don't know, something maybe :) :) :)

--- Assets/Scripts/UIDraggable.cs
+++ Assets/Scripts/UIDraggable.cs
@@ -9,7 +9,6 @@ public class UIDraggable : MonoBehaviour, IBeginDragHandler, IEndDragHandler, ID
     Transform _parent;
     int _siblingOrder;
 
-    GameObject _placeHolder;
 
     bool _startedDragging;
      Image _image;
@@ -23,26 +22,22 @@ public class UIDraggable : MonoBehaviour, IBeginDragHandler, IEndDragHandler, ID
     {
         _startedDragging = true;
         _startPosition = transform.position;
-        _image.raycastTarget = false;
+        if(_image != null) _image.raycastTarget = false;
         _parent = transform.parent;
         _siblingOrder = transform.GetSiblingIndex();
 
         transform.SetParent(_parent.parent);
         eventData.selectedObject = gameObject;
 
-        _placeHolder = new GameObject();
-        _placeHolder.transform.SetParent(_parent);
-        _placeHolder.transform.SetSiblingIndex(_siblingOrder);
     }
 
     public void OnEndDrag(PointerEventData eventData)
     {
         _startedDragging = false;
 		transform.position = _startPosition;
-        _image.raycastTarget = true;
+        if(_image != null) _image.raycastTarget = true;
         transform.SetParent(_parent);
         transform.SetSiblingIndex(_siblingOrder);
-        Destroy(_placeHolder);
     }
 
     public void OnDrag(PointerEventData eventData)
