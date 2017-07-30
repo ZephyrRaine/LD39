0c498a4ba9f8d08a4c997dba69034742bdadd33e
ZephyrRaine <zephyr.raine@gmail.com>
2017-07-30 23:42:59 +0200

    bubbles layout and stuff I guess

---
+++ Assets/BubblesHandler.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DIALOGUE_TYPES
+{
+	HUNGRY,
+	THIRSTY,
+	DIRTY,
+	PLAY,
+	DARK,
+	OVERWATER,
+	THANKS,
+	UPSET,
+	RANDOM
+}
+
+public class BubblesHandler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
